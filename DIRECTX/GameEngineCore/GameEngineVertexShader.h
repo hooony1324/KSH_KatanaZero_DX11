3047115c8bbea005f08baa1982c45590f98b1004
Cogason <hooony1324@gmail.com>
2022-06-29 18:25:56 +0900

    김성훈 : 인풋 레이아웃 업데이트

--- DIRECTX/GameEngineCore/GameEngineVertexShader.h
+++ DIRECTX/GameEngineCore/GameEngineVertexShader.h
@@ -18,6 +18,8 @@ public:
 	GameEngineVertexShader& operator=(const GameEngineVertexShader& _Other) = delete;
 	GameEngineVertexShader& operator=(GameEngineVertexShader&& _Other) noexcept = delete;
 
+	static GameEngineVertexShader* Load(std::string _Path);
+
 protected:
 
 private:
