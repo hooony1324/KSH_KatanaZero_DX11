04864e5e8850bb44ba3e3a3b4ef0050d755f244d
성훈 김 <hooony1324@gmail.com>
2022-08-19 12:28:54 +0900

    김성훈 : 임시 저장

--- DIRECTX/GameEngineContents/ContentsCore.cpp
+++ DIRECTX/GameEngineContents/ContentsCore.cpp
@@ -36,7 +36,7 @@ void ContentsCore::Start()
 	CreateLevel<PlayLevel>("PlayLevel");
 	CreateLevel<EndingLevel>("EndingLevel");
 	CreateLevel<TestLevel>("TestLevel");
-	ChangeLevel("TestLevel");
+	ChangeLevel("PlayLevel");
 
 	// Input Key
 	GameEngineInput::GetInst()->CreateKey("MouseLeft", VK_LBUTTON);
--- DIRECTX/GameEngineContents/TestActor.cpp
+++ DIRECTX/GameEngineContents/TestActor.cpp
@@ -14,10 +14,15 @@ TestActor::~TestActor()
 
 void TestActor::Start()
 {
+	GameContentsCustomRenderer* back = CreateComponent<GameContentsCustomRenderer>();
+	back->SetTexture("room_factory_2.png");
+	back->ScaleToTexture();
+
+
 	TRenderer = CreateComponent<GameContentsCustomRenderer>();
 	TRenderer->CreateFrameAnimationFolder("FanSpin", FrameAnimation_DESC{ "fanblade_spin", 0.05f, true });
 	TRenderer->ChangeFrameAnimation("FanSpin");
-	TRenderer->GetTransform().SetLocalScale({ 200, 200, 2 });
+	TRenderer->GetTransform().SetLocalScale({ 600, 600, 2 });
 	
 	TRenderer->Option.IsMask = 1;
 	TRenderer->SetMask("spr_lightcone_0.png");
